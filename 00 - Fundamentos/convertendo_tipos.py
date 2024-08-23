9d6f77b75a0206b3e8767cabff0a9f6384438dbb
iwebert <83651092+iwebert@users.noreply.github.com>
2024-08-23 15:56:00 -0300

    Update convertendo_tipos.py
    Acrescentei mais alguns testes feitos durante os estudos e descrevi através de comentários o que cada linha está efetuado.

--- 00 - Fundamentos/convertendo_tipos.py
+++ 00 - Fundamentos/convertendo_tipos.py
@@ -1,12 +1,25 @@
+#Conversão de valor decimal (float) para inteiro (int)
 print(int(1.97348728))
+
+#Conversão de String para inteiro (int)
 print(int("10"))
+
+#Conversão de String para decimal (float)
 print(float("10.10"))
+
+#Conversão de inteiro (int) para decimal (float)
 print(float(100))
 
-valor = 10
-valor_str = str(valor)
-print(type(valor))
-print(type(valor_str))
-
+#No exemplo abaixo é retornado um float, uma vez que foi usado apenas uma barra para a divisão
 print(100 / 2)
+
+#No exemplo abaixo é retornado um int, uma vez que foi usado duas barras para a divisão
 print(100 // 2)
+
+# Teste de erro (No exemplo abaixo é retornado erro uma vez que está tentando converter a string 'teste' para inteiro
+idade, nome = ('teste', 'João')
+idade = int(idade))
+
+# Teste de printar o tipo de variável (No caso abaixo é retornado int, uma vez que o valor indicado para a variável é um inteiro)
+idade = 10
+print(type(idade))
